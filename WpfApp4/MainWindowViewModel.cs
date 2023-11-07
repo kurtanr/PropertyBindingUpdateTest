e18a8119d3a876df1159c90db4270d8f9af8665c
Travis Nickles <ryochan7@protonmail.com>
2023-11-07 11:48:36 -0600

    Test repo

---
+++ WpfApp4/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private bool optionEnabled;
+        public bool OptionEnable
+        {
+            get => optionEnabled;
+            set
+            {
+                if (optionEnabled == value) return;
+
+                optionEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
